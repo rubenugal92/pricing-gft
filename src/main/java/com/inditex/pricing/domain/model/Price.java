a2c931e2457ceebacd27c4e13272df98cc2716fc
rubenugal92 <55811527+rubenugal92@users.noreply.github.com>
2025-08-08 19:32:32 +0200

    modify comment.java

--- src/main/java/com/inditex/pricing/domain/model/Price.java
+++ src/main/java/com/inditex/pricing/domain/model/Price.java
@@ -25,6 +25,6 @@ public class Price {
 // En un escenario más complejo, aquí se incluirían métodos de negocio, como validaciones, comprobaciones etc.
 // como por ejemplo decisiones sobre qué precio aplicar en función de la prioridad, pero eso ya lo hace la misma query y si por ejemplo mañana cambias a otra base de datos,
 // seguramente siempre se va a querer delegarlo a la query aunque tengamos un metodo aqui para no depender de si cambiamos o no de base de datos, por lo que sería redundante.
-// En este caso concreto, he optado por mantener el modelo simple, siguiendo el principio de mantener el dominio limpio hasta que
-// emerjan reglas de negocio que realmente justifiquen el tener que poner métodos de dominio.
+// En este caso concreto, he optado por mantener el modelo simple, siguiendo el principio YAGNI (mantener el dominio limpio hasta que
+// emerjan reglas de negocio que realmente justifiquen el tener que poner métodos de dominio).
 
